2490af680d4de25963798c09d88b1a05defd4ed7
BrunoMollo <dio.mollo.bruno@gmail.com>
2019-03-08 11:55:24 -0300

    Script acelerometro y tres cuadraditos

---
+++ Crazy Jungle/Assets/Scripts c#/Movimento_acelerometro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento_acelerometro : MonoBehaviour
+{
+
+    [SerializeField]
+    private float FuerzaLateral=1;
+
+    private Rigidbody2D rb;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.velocity = new Vector2(FuerzaLateral * Input.acceleration.x, rb.velocity.y);
+
+
+        Debug.Log(Input.acceleration.x.ToString());
+
+        
+    }
+}
