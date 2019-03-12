5d5464b07c3f4ac096844fc7c07a3fe60a798924
BrunoMollo <dio.mollo.bruno@gmail.com>
2019-03-12 15:36:28 -0300

    Script Movimiento Modificado
    Ahora se puede testear con el teclado o con la inclinacion del celular(En el scipt hay una cajita que tienen que tildar o destildar)

--- Crazy Jungle/Assets/Scripts c#/Character/Movimento_acelerometro.cs
+++ Crazy Jungle/Assets/Scripts c#/Character/Movimento_acelerometro.cs
@@ -1,39 +1,76 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movimento_acelerometro : MonoBehaviour
 {
-    [Header("Floats")]
+    //Variables para ajustar como se mueve
+    [Header("Fuerzas Movimiento:")]
     [SerializeField]
-    private float FuerzaLateral = 1;
+    private float FuerzaLateral_PC = 1;
+    [SerializeField]
+    private float FuerzaLateral_Celular = 1;
 
 
+    //Bool que indica si se va a usar el teclado o Unity Remote
+    [Header("Input: ")]
+    [SerializeField]
+    private bool UnityRemote_Conectado=false;
 
+
+    //Pedidos en Start()
     private Rigidbody2D rb;
+    private Vector3 InclinacionInicial;
+
+
+  
 
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        InclinacionInicial = Input.acceleration;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        move();
+        if (UnityRemote_Conectado)
+        {
+            moveCelular();
+            
+        }
 
-        
+        else
+        {
+            movePC();
+        }
+
+    
     }
 
 
-    // Funcion para el movimiento del personaje
-    private void move()
+
+
+    //Funcion para mover el personaje inclinando el celular
+    private void moveCelular()
     {
-        float move = Input.GetAxisRaw("Horizontal");
+        float Despalazamiento_X = (FuerzaLateral_Celular * Input.acceleration.x) - InclinacionInicial.x;
+
+        rb.velocity = new Vector2(Despalazamiento_X, rb.velocity.y);
+    }
 
 
-        rb.velocity = new Vector2(FuerzaLateral * move, rb.velocity.y);
+
+    // Funcion para mover el personaje con el teclado
+    private void movePC()
+    {   
+        float Direccion = Input.GetAxisRaw("Horizontal");
+
+
+        rb.velocity = new Vector2(FuerzaLateral_PC * Direccion, rb.velocity.y);
     }
 }
