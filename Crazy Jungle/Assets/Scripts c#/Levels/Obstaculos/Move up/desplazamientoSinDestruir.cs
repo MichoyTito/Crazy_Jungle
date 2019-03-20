3b7464ccfe5fa56439b9daef8f209e8381bf3fca
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-03-19 21:09:56 -0300

    Ajustes en nivel1

---
+++ Crazy Jungle/Assets/Scripts c#/Levels/Obstaculos/Move up/desplazamientoSinDestruir.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class desplazamientoSinDestruir : MonoBehaviour
+{
+    [Header("Propiedades")]
+    [SerializeField]
+    private float Velocidad = 1f;
+    [SerializeField]
+    private float aceleracion = 1f;
+
+    private Transform rama;
+
+
+    private void Start()
+    {
+        rama = GetComponent<Transform>();
+
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        EmpujarRama();
+
+    }
+
+
+
+
+    private void EmpujarRama()
+    {
+
+        float Rapidez = Velocidad + aceleracion * Time.timeSinceLevelLoad;
+
+        rama.Translate(Vector3.up * Rapidez * Time.deltaTime);
+
+    }
+
+
+}
