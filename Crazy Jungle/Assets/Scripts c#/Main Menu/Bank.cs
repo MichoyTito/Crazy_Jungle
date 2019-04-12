1bf5d52bacad6d0dff79118d4f9002014c667ac2
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-04-12 18:03:06 -0300

    Survival

---
+++ Crazy Jungle/Assets/Scripts c#/Main Menu/Bank.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bank : MonoBehaviour
+{
+    [SerializeField]
+    private float money;
+
+    [Header("Texts")]
+    [SerializeField]
+    private Text moneyText;
+
+    public void Start()
+    {
+        money =  money + PlayerPrefs.GetFloat("Coins");
+        moneyText.text = money.ToString("F0");
+    }
+}
--- Crazy Jungle/Assets/Scripts c#/Survival/lose.cs
+++ Crazy Jungle/Assets/Scripts c#/Survival/lose.cs
@@ -20,8 +20,23 @@ public class lose : MonoBehaviour
     private Text finalScore;
     [SerializeField]
     private Text finalCoins;
+    [SerializeField]
+    private Text bestScore;
 
     private float coinsEarned;
+
+
+    private void Start()
+    {
+        if(PlayerPrefs.GetFloat("Coins") == 0)
+        {
+            PlayerPrefs.SetFloat("Coins", 0);
+        }
+        if(PlayerPrefs.GetFloat("bestScore") == 0)
+        {
+            PlayerPrefs.SetFloat("bestScore", 0);
+        }
+    }
 
     public void Dead()
     {
@@ -33,6 +48,15 @@ public class lose : MonoBehaviour
         float scoreFloat = float.Parse(finalScore.text);
         coinsEarned = scoreFloat * 0.01f;
         finalCoins.text = coinsEarned.ToString("F0");
+
+        PlayerPrefs.SetFloat("Coins", PlayerPrefs.GetFloat("Coins") + coinsEarned);
+
+        if(scoreFloat > PlayerPrefs.GetFloat("bestScore"))
+        {
+            PlayerPrefs.SetFloat("bestScore", scoreFloat);
+        }
+        bestScore.text = PlayerPrefs.GetFloat("bestScore").ToString();
+
     }
 
     public void onClickMenu()
