0d834d8773bc8c77619dc48cfe80a5e5729032be
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-04-11 21:17:06 -0300

    Survival

--- Crazy Jungle/Assets/Scripts c#/Survival/lose.cs
+++ Crazy Jungle/Assets/Scripts c#/Survival/lose.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class lose : MonoBehaviour
 {
     [SerializeField]
     private GameManager gameManage;
 
+    [Header("Objects")]
     [SerializeField]
     private GameObject losePanel;
+
+    [Header("Texts")]
+    [SerializeField]
+    private Text score;
+    [SerializeField]
+    private Text finalScore;
+    [SerializeField]
+    private Text finalCoins;
+
+    private float coinsEarned;
 
     public void Dead()
     {
         losePanel.SetActive(true);
         gameManage.PauseLevel();
+        finalScore.text = score.text;
+
+        //Coins calculating
+        float scoreFloat = float.Parse(finalScore.text);
+        coinsEarned = scoreFloat * 0.01f;
+        finalCoins.text = coinsEarned.ToString("F0");
     }
 
     public void onClickMenu()
