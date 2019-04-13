d64ec8adaa9f7fb5b247ed3571faebaeb2e3199b
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-04-12 21:36:44 -0300

    Survival

--- Crazy Jungle/Assets/Scripts c#/Survival/ramas.cs
+++ Crazy Jungle/Assets/Scripts c#/Survival/ramas.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ramas : MonoBehaviour
 {
+    [Header("Pick Ups")]
+    [SerializeField]
+    private GameObject banana;
+
+    [Header("Obstaculos")]
+    [SerializeField]
+    private GameObject coco;
+
     [Header("Ramas")]
     [SerializeField]
     private GameObject rama1;
@@ -24,24 +32,29 @@ public class ramas : MonoBehaviour
     private GameObject cloneRama2;
     private GameObject cloneRama3;
 
+    private GameObject cloneCoco;
+    private GameObject cloneCoco2;
+    private GameObject cloneBanana;
+
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("spawnRama1", 3, Random.Range(2, 4));
         InvokeRepeating("spawnRama2", 6, Random.Range(2, 4));
         InvokeRepeating("spawnRama3", 9, Random.Range(1.5f, 2));
+
+        InvokeRepeating("spawnCoco", 13, 7);
+        InvokeRepeating("spawnCoco2", 15, 13);
+
+        InvokeRepeating("spawnBanana", 4, 7);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void spawnRama1()
     {
+        float y = Random.Range(-12, -5);
 
-        cloneRama1 = Instantiate(rama1, new Vector3(x1, -5, 0), Quaternion.identity);
+        cloneRama1 = Instantiate(rama1, new Vector3(x1, y, 0), Quaternion.identity);
         cloneRama1.transform.parent = parent.transform;
 
         if(x1 == 1.55f)
@@ -58,8 +71,8 @@ public class ramas : MonoBehaviour
     }
     private void spawnRama2()
     {
-
-        cloneRama2 = Instantiate(rama2, new Vector3(x2, -8, 0), Quaternion.identity);
+        float y = Random.Range(-12, -5);
+        cloneRama2 = Instantiate(rama2, new Vector3(x2, y, 0), Quaternion.identity);
         cloneRama2.transform.parent = parent.transform;
 
         if (x2 == 1.55f)
@@ -75,8 +88,8 @@ public class ramas : MonoBehaviour
     }
     private void spawnRama3()
     {
-
-        cloneRama3 = Instantiate(rama3, new Vector3(x3, -6, 0), Quaternion.identity);
+        float y = Random.Range(-12, -5);
+        cloneRama3 = Instantiate(rama3, new Vector3(x3, y, 0), Quaternion.identity);
         cloneRama3.transform.parent = parent.transform;
 
         if (x3 == -1.88f)
@@ -90,5 +103,26 @@ public class ramas : MonoBehaviour
         }
 
         Destroy(cloneRama3, 6);
+    }
+    private void spawnCoco()
+    {
+        float x = Random.Range(-2.15f, 2.1f);
+
+        cloneCoco = Instantiate(coco, new Vector3(x, -8, 0), Quaternion.identity);
+        cloneCoco.transform.parent = GameObject.Find("Background").transform;
+    }
+    private void spawnCoco2()
+    {
+        float x = Random.Range(-2.15f, 2.1f);
+
+        cloneCoco2 = Instantiate(coco, new Vector3(x, -8, 0), Quaternion.identity);
+        cloneCoco2.transform.parent = GameObject.Find("Background").transform;
+    }
+    private void spawnBanana()
+    {
+        float x = Random.Range(-2, 2);
+
+        cloneBanana = Instantiate(banana, new Vector3(x, -8, 0), Quaternion.identity);
+        cloneBanana.transform.parent = GameObject.Find("Background").transform;
     }
 }
